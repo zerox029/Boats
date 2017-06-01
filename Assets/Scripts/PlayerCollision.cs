bd68cf155a0a439a574c808cbcc1771b950533f8
zerox029 <zerox029@gmail.com>
2017-06-01 18:07:27 -0400

    Merged 2 scripts into playerMovement

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Debug.Log("Game Over");
+            Time.timeScale = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    public GameObject boat;
+
+	// Update is called once per frame
+	void Update () {
+        SendRaycast();
+	}
+
+    private void SendRaycast()
+    {
+        Ray ray = new Ray(boat.transform.position, -Vector3.up);
+        RaycastHit hit;
+
+        Physics.Raycast(ray, out hit);
+        Debug.DrawRay(boat.transform.position, -Vector3.up * 10, Color.red);
+
+        if(hit.transform.tag == "Obstacle")
+        {
+            Debug.Log("+1 Point");
+        }
+    }
+}
